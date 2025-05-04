eae543538b6213ed176cece5b5bf4a7511850ef1
Cesar Palma Aguilar <czarv10@gmail.com>
2025-05-04 19:25:05 -0400

    Updated plant2 to use Q8.8 fixed point arithmetic.

--- plant2.vhd
+++ plant2.vhd
@@ -1,37 +1,35 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use IEEE.fixed_pkg.all;
 
 entity plant2 is
     Port ( reset : in STD_LOGIC;
            clk : in STD_LOGIC;
-           temp_out : out STD_LOGIC_VECTOR (7 downto 0);
-           control_in : in STD_LOGIC_VECTOR (7 downto 0);
+           temp_out : out STD_LOGIC_VECTOR (15 downto 0);
+           control_in : in STD_LOGIC_VECTOR (15 downto 0);
            enable : in STD_LOGIC);
 end plant2;
 
 architecture Behavioral of plant2 is
-    signal tout_reg : signed(7 downto 0) := (others => '0');  -- T(t)
-    constant tparameter : integer := 100;
-    -- signal tout_pipe : signed(7 downto 0) := (others => '0');
+    signal tout_reg : sfixed(7 downto -8) := to_sfixed(0.0, 7, -8);
+    constant TPARAMETER : sfixed(7 downto -8) := to_sfixed(100.0, 7, -8);
 begin
     process(clk,reset)
-        variable diff : signed(7 downto 0);
-        variable temp_product : signed(15 downto 0);
-        variable delta : signed(7 downto 0);    
+        variable diff : sfixed(7 downto -8);
+        variable delta : sfixed(7 downto -8);
+        
         begin
         if reset = '1' then
-            tout_reg <= (others => '0');
+            tout_reg <= to_sfixed(0.0, 7, -8);
         elsif rising_edge(clk) then
             if enable = '1' then
-                diff := signed(to_signed(TPARAMETER, 8)) - tout_reg;
-                temp_product := signed(control_in) * diff;
-                delta := resize(shift_right(temp_product, 7), 8);
+                diff := TPARAMETER - tout_reg;
+                delta := resize(to_sfixed(control_in, 7, -8) * diff, 7, -8);
+                delta := delta / to_sfixed(128.0, 7, -8);
                 tout_reg <= tout_reg + delta;
-                -- tout_pipe <= tout_reg;
             end if;
         end if;
     end process;
-    -- temp_out <= std_logic_vector(tout_pipe);
     temp_out <= std_logic_vector(tout_reg); 
 end Behavioral;
